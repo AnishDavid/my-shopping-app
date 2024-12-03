d972a65c8c50c5056b8ba88a25165769ee9d08b8
AnishDavid <antonymosses156@gmail.com>
2024-12-04 03:33:35 +0530

    Updates

---
+++ static/js/243.570d75e6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_shopping_app=self.webpackChunkmy_shopping_app||[]).push([[243],{243:(s,p,e)=>{e.r(p),e.d(p,{default:()=>h});var a=e(579);const h=()=>(0,a.jsx)(a.Fragment,{children:(0,a.jsx)("h1",{children:"Hi, This is Contact Us page"})})}}]);
+//# sourceMappingURL=243.570d75e6.chunk.js.map
